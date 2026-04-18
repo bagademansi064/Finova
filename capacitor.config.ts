bfa67964c59345417b88ef161a5bcebf44102687
Pranav Dubey <pranavdubey2024@outlook.com>
2026-04-18 12:09:33 +0530

    stock discussion working

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,9 +3,12 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.finova.app',
+  appId: 'com.finova.app.dev',
   appName: 'Finova',
   webDir: 'out',
+  server: {
+    cleartext: true,
+  },
   plugins: {
     SplashScreen: {
       launchShowDuration: 2000,
